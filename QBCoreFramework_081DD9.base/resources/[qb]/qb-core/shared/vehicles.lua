faa2af1718d59b8bd13be45f836035cb7c2c78e5
Moost <moost.tv@gmail.com>
2024-10-28 12:54:49 +0100

    Add vehicle : explorer + add GTAVRemastered

--- QBCoreFramework_081DD9.base/resources/[qb]/qb-core/shared/vehicles.lua
+++ QBCoreFramework_081DD9.base/resources/[qb]/qb-core/shared/vehicles.lua
@@ -3,6 +3,16 @@ QBShared.Vehicles = QBShared.Vehicles or {}
 
 local Vehicles = {
     --- Compacts (0)
+    --- 
+    {
+        model = 'Explorer',        -- This has to match the spawn code of the vehicle
+        name = 'Explorer',         -- This is the display of the vehicle
+        brand = 'Ford',     -- This is the vehicle's brand
+        price = 4000,          -- The price that the vehicle sells for
+        category = 'suvs', -- Catgegory of the vehilce, stick with GetVehicleClass() options https://docs.fivem.net/natives/?_0x29439776AAA00A62
+        type = 'automobile',   -- Vehicle type, refer here https://docs.fivem.net/natives/?_0x6AE51D4B & here https://docs.fivem.net/natives/?_0xA273060E
+        shop = 'pdm',          -- Can be a single shop or multiple shops. For multiple shops for example {'shopname1','shopname2','shopname3'}
+    },
     {
         model = 'asbo',        -- This has to match the spawn code of the vehicle
         name = 'Asbo',         -- This is the display of the vehicle
