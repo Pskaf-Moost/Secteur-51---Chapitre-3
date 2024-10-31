e5ad8a05920932cb70849dd2c3c7433a320b8c16
Moost <moost.tv@gmail.com>
2024-10-31 17:12:52 +0100

    Add jim-mechanics : related change : ox-lib, qb inv, qb core

---
+++ QBCoreFramework_081DD9.base/resources/[qb]/qb-stashes/config.lua
@@ -0,0 +1,18 @@
+Config = {}
+
+Config.PoliceOpen = true -- Can Police open all stashes
+
+Config.Stashes = {
+    -- ["geminimansion"] = {
+    --     stashName = "geminimansion",
+    --     coords = vector3(-1730.5, 358.6, 88.73), 
+    --     requirecid = true,
+    --     jobrequired = false,
+    --     gangrequired = false,
+    --     gang = "",
+    --     job = "",
+    --     cid = {"MAH44286"},  
+    --     stashSize = 1250000,
+    --     stashSlots = 125, 
+    -- },
+}
